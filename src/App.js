c46a730a4196832860306c41745cfce25cde629c
Henry Bae <69275685+BaeHenryS@users.noreply.github.com>
2023-06-02 21:43:05 -0400

    Henry Fleury test 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js HELLO FLEURY WAS HERE</code> and save to reload.
+          Edit <code>src/App.js HELLO FLEURY WAS HERE and Henry</code> and save to reload.
         </p>
         <a
           className="App-link"
