69f6cea9409fed01f12ca7b6ce55017099e6e960
Henry Bae <69275685+BaeHenryS@users.noreply.github.com>
2023-07-08 18:13:57 -0700

    Getting Database working V1

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore'
 
 import { getAuth } from "firebase/auth";
 
@@ -27,3 +28,4 @@ const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
 export { auth };
+export const db = getFirestore(app);
